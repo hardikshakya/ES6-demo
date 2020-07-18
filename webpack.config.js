39eb31d2e726695f0c5412adba0b2db6ec788f5f
hardikshakya <hardikshakya1997@gmail.com>
2020-07-18 23:36:03 +0530

    T3: Converting ES6 to ES5

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,16 @@ module.exports = {
   entry: './assets/js/scripts.js',
   output: {
     filename: './js/bundle.js'
+  },
+
+  module:{
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
   }
+
 };
